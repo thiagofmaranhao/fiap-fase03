fac2e75a7e1def05e2c8e28c0cd7cda03ab145c4
Leandro Franchi <leandro.franchi@gmail.com>
2022-03-17 20:31:53 -0300

    Não me auentei e fiz mais um pouco, continuo amanha!

---
+++ abc-tech-service/src/main/java/br/com/fiap/abctechservice/security/JwtUserDetailService.java
@@ -0,0 +1,36 @@
+package br.com.fiap.abctechservice.security;
+
+import br.com.fiap.abctechservice.model.User;
+import br.com.fiap.abctechservice.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class JwtUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public JwtUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findFirstByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                new ArrayList<>() // Roles
+
+        );
+    }
+
+
+}
