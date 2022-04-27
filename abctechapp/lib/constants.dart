53c0416a7043746f0918fb062de011a18c2aac81
Leandro Franchi <leandro.franchi@gmail.com>
2022-04-26 21:11:10 -0300

    Exclusão do App.

--- abctechapp/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-class Constants {
-  static const url =
-      "http://abctechserviceapi-env.eba-hermxgp3.us-east-2.elasticbeanstalk.com";
-}
--- abctechapp/lib/pages/home_bind.dart
+++
@@ -1,9 +0,0 @@
-import 'package:abctechapp/controller/assistance_controller.dart';
-import 'package:get/get.dart';
-
-class HomeBind extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<AssistanceController>(() => AssistanceController());
-  }
-}
